626314536bbdb1b14bc0ce0d7f0fb615f9c55327
mole <andreymole@mail.ru>
2021-02-13 23:01:11 +0300

    4

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(serveStatic(__dirname))
 app.get('/bsiua523hntv2.txt',function(req,res) {
     res.send('')
 })
+app.get('*', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+});
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
