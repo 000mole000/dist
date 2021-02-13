2dba7c24c431866e55004df98213b073948c51ed
mole <andreymole@mail.ru>
2021-02-13 22:57:47 +0300

    3

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-var expressVue = require("express-vue");
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
-// app.use(serveStatic(__dirname))
-expressVue.use(app).then(() => {
-    app.get('/bsiua523hntv2.txt',function(req,res) {
-        res.send('')
-    })
-});
+app.use(serveStatic(__dirname))
+app.get('/bsiua523hntv2.txt',function(req,res) {
+    res.send('')
+})
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
